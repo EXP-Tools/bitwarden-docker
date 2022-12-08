842ea081eeeb4cdef100dbd5ca53ce9e18fbccff
exp.liao <289065406@qq.com>
2022-12-08 13:33:30 +0800

    修正日志路径

--- bin/enable_fail2ban.sh
+++ bin/enable_fail2ban.sh
@@ -10,7 +10,7 @@ if [ -z "${INSTALL_DIR}" ] ; then
   INSTALL_DIR="/usr/local/bitwarden-docker"
 fi
 
-sed -i s@/usr/local/bitwarden-docker@${INSTALL_DIR}@g ${INSTALL_DIR}/fail2ban/jail.d/bitwarden.local
+sed -i s@/var/log@${INSTALL_DIR}/logs@g ${INSTALL_DIR}/fail2ban/jail.d/bitwarden.local
 
 cp ${INSTALL_DIR}/fail2ban/jail.d/bitwarden.local /etc/fail2ban/jail.d/bitwarden.local
 cp ${INSTALL_DIR}/fail2ban/filter.d/bitwarden.local /etc/fail2ban/filter.d/bitwarden.local
